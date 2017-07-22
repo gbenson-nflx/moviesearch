1dc663e7a094c3c90cf4442c13b564081a2dda4a
Gregory Benson <gbenson@netflix.com>
2017-07-21 21:58:53 -0700

    Adding baseline espresso test case

---
+++ app/src/androidTest/java/moviesearch/netflix/AppTitleEspressoTest.java
@@ -0,0 +1,28 @@
+package moviesearch.netflix;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import moviesearch.netflix.ui.MovieListActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class AppTitleEspressoTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MovieListActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        String name = mActivityRule.getActivity().getString(R.string.app_name);
+        onView(withText(name)).check(matches(isDisplayed()));
+    }
+}
