c6e82953bac0b9d7cd351b28da2e758bf939573c
Gregory Benson <gbenson@netflix.com>
2017-07-21 21:09:06 -0700

    Adding click listener for movie item action

--- app/src/main/java/moviesearch/netflix/ui/MovieListAdapter.java
+++ app/src/main/java/moviesearch/netflix/ui/MovieListAdapter.java
@@ -1,7 +1,9 @@
 package moviesearch.netflix.ui;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,16 +11,16 @@ import java.util.List;
 import moviesearch.netflix.model.Movie;
 import moviesearch.netflix.ui.MovieListItemView.MovieListItemViewHolder;
 
-public class MovieListAdapter extends RecyclerView.Adapter {
+class MovieListAdapter extends RecyclerView.Adapter {
 
     private List<Movie> movies;
 
-    public void clearMovies() {
+    void clearMovies() {
         this.movies = null;
         notifyDataSetChanged();
     }
 
-    public void addMovies(List<Movie> movies) {
+    void addMovies(List<Movie> movies) {
         if (this.movies == null) {
             this.movies = new ArrayList<>();
         }
@@ -33,7 +35,17 @@ public class MovieListAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ((MovieListItemViewHolder) holder).getItemView().bindMovie(getMovie(position));
+        final MovieListItemView item = ((MovieListItemViewHolder) holder).getItemView();
+
+        final Movie movie = getMovie(position);
+        item.bindMovie(movie);
+
+        item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(item.getContext(), movie.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
